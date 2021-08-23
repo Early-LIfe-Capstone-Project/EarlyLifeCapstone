582d5f7cd822c5764b1dfa1b26022bc2fdf197d5
Dylan Solin <solin.dylan@gmail.com>
2021-08-23 22:50:47 +0200

    first commit

---
+++ app/src/main/java/com/example/earlylife/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.earlylife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
