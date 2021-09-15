e94eaf85b18f3f3998cb210205eb4a8e177f55c7
tiffany <tiffany.yuan25@gmail.com>
2021-09-15 23:14:48 +0200

    New splash (start up) screen

--- app/src/main/java/com/example/earlylife/MainActivity.kt
+++ app/src/main/java/com/example/earlylife/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.earlylife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.TextView
+import androidx.core.os.postDelayed
 import androidx.fragment.app.Fragment
 import com.example.earlylife.fragments.HomeFragment
 import com.example.earlylife.fragments.QuiltDetailsFragment
@@ -11,38 +14,17 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private val homeFragment = HomeFragment()
-    private val quiltDetailsFragment = QuiltDetailsFragment()
-    private val settingsFragment = SettingsFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        replaceFragment(homeFragment)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
 
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId){
-                R.id.ic_home -> replaceFragment(homeFragment)
-                R.id.ic_quilt_details -> replaceFragment(quiltDetailsFragment)
-
-            }
-            true
-        }
-
-        val actionBarView = findViewById<MaterialToolbar>(R.id.actionBarView)
-        setSupportActionBar(actionBarView)
-
-
-    }
-
-    private fun replaceFragment (fragment: Fragment) {
-        if (fragment != null) {
-            val transaction= supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, fragment)
-            transaction.commit()
-        }
     }
 
     }
