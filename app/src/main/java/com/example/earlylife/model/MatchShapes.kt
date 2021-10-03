324198e2ebfe123ce710e816ee88661fc1812f5b
Dylan-Solin <55622633+Dylan-Solin@users.noreply.github.com>
2021-10-03 21:38:17 +0200

    Added More Classes for API implementation

---
+++ app/src/main/java/com/example/earlylife/model/MatchShapes.kt
@@ -0,0 +1,10 @@
+package com.example.earlylife.model;
+
+data class MarchShapes(
+    val activityName: String,
+    val activityID: String,
+    val correct: String,
+    val date: String,
+    val timeOnTask: String
+)
+
---
+++ app/src/test/java/com/example/earlylife/DataHandlerTest.kt
@@ -0,0 +1,15 @@
+package com.example.earlylife
+
+import junit.framework.TestCase
+import org.junit.Test
+
+class DataHandlerTest {
+    @Test
+    fun testHandle() {
+        val data = DataHandler()
+
+        data.handle()
+
+    }
+
+}
