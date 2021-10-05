fbfd85aa9d544661b47146d727920f6201c8b3d4
Zahra <mhmzah023@myuct.ac.za>
2021-10-05 19:54:43 +0200

    Added some functionality to the QuiltDetailsFragment

--- app/src/main/java/com/example/earlylife/fragments/QuiltDetailsFragment.kt
+++ app/src/main/java/com/example/earlylife/fragments/QuiltDetailsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.earlylife.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import com.example.earlylife.R
+import android.widget.ArrayAdapter
+import com.example.earlylife.MainActivity
 
 class QuiltDetailsFragment : Fragment() {
 
@@ -13,8 +17,14 @@ class QuiltDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_quilt_details, container, false)
+
+        val activityNames: List<String> = (activity as MainActivity).activityList.getActivityNames()
+        val listView = view.findViewById(R.id.activity_info_list) as ListView
+        val adapter: ArrayAdapter<String> = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, activityNames)
+        listView.adapter = adapter
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quilt_details, container, false)
+        return view
     }
 
 }
